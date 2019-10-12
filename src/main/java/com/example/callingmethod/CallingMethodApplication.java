ab046e2c07b5f5b394fe7f61da4d71f705c57393
Raja Sundaram <sundarsy@gmail.com>
2019-10-12 19:19:38 +0800

    first commit :Calling methods

---
+++ src/main/java/com/example/callingmethod/CallingMethodApplication.java
@@ -0,0 +1,14 @@
+package com.example.callingmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingMethodApplication.class, args);
+		System.out.print("Preethy dear!!!");
+	}
+
+}
